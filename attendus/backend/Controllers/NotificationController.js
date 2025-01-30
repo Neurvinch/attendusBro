58b6eefe6b195254ef7ed70462787c0087af84b7
neuervinch <npandian515@gmail.com>
2025-01-31 02:13:19 +0530

    hh

--- attendus/backend/Controllers/NotificationController.js
+++ attendus/backend/Controllers/NotificationController.js
@@ -1,1 +1,20 @@
 const router = require("express").Router();
+const identifier = require("../MidlleWare/identification");
+const UserModel = require("../Models/UserModel");
+
+
+router.get('/', identifier(['student']) ,   async (req , res) =>{
+
+    try {
+        const notification = await UserModel.findById({
+            userId : req.user._id
+        }).sort('-createdAt')
+
+        res.json({sucess : true , data : notification})
+        
+    } catch (error) {
+        
+        res.json({sucess : false , message : error.message})
+    }
+
+})
