0e582cd35b31beee1fdffe00443942252c68f321
neuervinch <npandian515@gmail.com>
2025-02-02 14:33:20 +0530

    comp 034

--- attendus/frontend/src/App.jsx
+++ attendus/frontend/src/App.jsx
@@ -1,13 +1,24 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./Pages/Login"
 import './App.css'
-import Login from './Pages/Login'
+
 
 function App() {
+  <Router>
+    <Navbar/>
+    <Routes>
+      <Route path ="/" element={<Home/>} />
+      <Route  path = "/login" elemnt = {<Login/>} />
+      <Route path = "/signup" element = {<Signup />} />
+    </Routes>
+  
+  </Router>
 
   return (
-    <>
-      <Login/>
-    </>
+    
   )
 }
 
