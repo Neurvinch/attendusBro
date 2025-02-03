59854dab5e90e4ccd3bc9ec703d1eb8443a9fd50
neuervinch <npandian515@gmail.com>
2025-02-03 23:20:11 +0530

    mmm

--- attendus/frontend/src/context/AuthContext.jsx
+++ attendus/frontend/src/context/AuthContext.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import api from '../api';
 
-const AuthContext = () => {
+const AuthContext = createContext() 
+const AuthProvider = ({children}) => {
+
+    const [user , setUser] = useState(null);
+    const [loading , setLoading] = usestate(true);
+
+    useEffect( () =>{
+        const checkAuth = async () =>{
+            try {
+                const token = localStorage.getItem('token');
+                if(token){
+                    const response = await api.getProfile();
+                    setUser(response.data);
+                }
+                
+            } catch (error) {
+                console.error(error);
+                logout();
+
+                
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        checkAuth();
+    } ,[])
+
+            const login = async (credentials) =>{
+                try {
+                    const res = await api.login(credentials);
+                    localStorage.setItem('token' , res.data.token);
+                    setUser(res.data.user);
+                    
+                } catch (error) {
+                     console.error(error);
+                }
+
+            }
+
+            const logout = () =>{
+                localStorage.removeItem('token');
+                setUser(null);
+            }
+
   return (
-    <div>AuthContext</div>
+    <AuthContext.provider value={{user , login , logout , loading}}>
+        {children}
+    </AuthContext.provider>
   )
 }
 
-export default AuthContext
+export const useAuth = () =>{
+    const context = useContext(AuthContext);
+    if(!context) throw new Error('useAuth must be used within an AuthProvider');
+    return context;
+};
