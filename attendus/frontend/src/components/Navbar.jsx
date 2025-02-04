836bbb8d1884e09ae359621c33786aa43fceb7e2
neuervinch <npandian515@gmail.com>
2025-02-04 18:03:21 +0530

    good dae

--- attendus/frontend/src/components/Navbar.jsx
+++ attendus/frontend/src/components/Navbar.jsx
@@ -6,20 +6,44 @@ const Navbar = () => {
   return (
     <div>
 
-        <Link  to = '/'  >   Home</Link>
+        <Link  to = '/'  >   AttendUS</Link>
+        {
+          user? (
+            <>
+             <Link to= '/dashboard' >
+               Dashboard
+             </Link>
 
-        {user ? (
+         
+         
+          {(user.roles === 'staff' || user.roles ==="hod") && (
+            <Link to ='/markAttendance' >
+              Mark Attendance
+            </Link>
+          )}
+
+        <Link to= '/profile' >
+           Profile
+        </Link>
+
+        <button
+         onClick={logout}
+         >
+            Logout
+        </button>
+        </>
+          ) :(
             <>
-              <Link to =' /dashboard'> Dashboard </Link>
-              <button onClick={logout} >Logout</button>
+            <Link to = '/login'>
+            Login
+              </Link>
+              <Link to = '/signup'>
+              
+              Sigup</Link>
             </>
-        ) : (
-            <>
-               <Link to= '/login' > Login</Link>
-               <Link to ='/signup' > Signup</Link>
-             </>
-        )}
-    </div>
+          
+            )}
+       </div>
   )
 }
 
--- attendus/frontend/src/components/TimetableView.jsx
+++ attendus/frontend/src/components/TimetableView.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const TimetableView = () => {
+const TimetableView = ({schedule}) => {
+    const days = ['monday' , 'tuesday' , 'wednesday' , 'thursday' , 'friday' , 'saturday' ]
+
   return (
-    <div>TimetableView</div>
+
+
+    <>
+       <h2>Time Table</h2>
+       {days.map( (day) =>(
+        <div  key ={day}>
+        <h4>{day}</h4>
+
+        {schedule[day]?.map( ( subject , index) =>(
+            <div key={index} >
+                <h5>{subject}</h5>
+            </div>
+        ))}
+        </div>
+       ))}
+    </>
   )
 }
 
