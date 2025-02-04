81e9aab5a100461eb41ec714104261d01654c394
neuervinch <npandian515@gmail.com>
2025-02-04 15:39:14 +0530

    some

--- attendus/frontend/src/Pages/Dashboard.jsx
+++ attendus/frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AttendanceSummary from '../components/AttendanceSummary';
+import TimeTableView from '../components/TimeTableView';
+import { useAuth } from '../context/AuthContext';
+import api from '../api';
 
 const Dashboard = () => {
+    const {user} = useAuth();
+    const [attendance , setAttendance] = useState([]);
+    const [timeTable ,setTimeTable] = useState( {});
+
+    useEffect(() =>{
+      const fetchAttendance  = async () =>{
+        if(user?.roles === 'student'){
+          try { 
+            const res = await api.get(`/attendance/${user.id}`);
+            setAttendance(res.data)
+            
+          } catch (error) {
+             console.log(' error fetching attendance' , error);
+          }
+        }
+       }
+       fetchAttendance();
+    },[ user] );
+
+     useEffect( () =>{
+      const fetchtimeTable  = async () =>{
+        try {
+         const endpoint = user?.roles ==='student ' ?
+         `/timetable/class/${user.class}` 
+         : `/timetable/staff/${user._department}`;
+         const res= await api.get(endpoint);
+         setTimeTable(res.data) 
+          
+        } catch (error) {
+           console.log(' error fetching time table' , error);
+        }
+         
+      }
+      fetchtimeTable();
+     } , [ user]);
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Welcome, {user?.rollNo }  ({user?.roles})</h1>
+
+      {user.roles ==='student'  && (
+        <>
+        <AttendanceSummary data={attendance} />
+        <TimeTableView  schedule = {timeTable} />
+        </>
+      )} 
+      
+          
+            { (user?.roles === 'staff' || user?.roles === 'hod') &&(
+              <>
+              <h2> Actions</h2>
+                 <a href= '/markAttendance' >
+                  Mark Attendance
+                 </a>
+                 < a  href = "/uploadMarks"> 
+                  Upload Marks
+                 </a>
+
+                 <Timetable   schedule = {timeTable}  />
+              
+              </>
+            )}
+            
+             
+      
+          </div>
   )
 }
 
--- attendus/frontend/src/components/AttendnaceMarking.jsx
+++ attendus/frontend/src/components/AttendnaceMarking.jsx
@@ -1,8 +1,106 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+import api from '../api';
 
 const AttendnaceMarking = () => {
+  const {user} = useAuth();
+  const [students ,setStudens] = useState([])
+  const [date, setDate] = useState( new Date().toISOString().split('T')[0] )
+ 
+   useEffect( () =>{
+     const fetchStudents = async() =>{
+      try {  
+        const res = await api.get(
+          `/students?department = ${user.department}`
+        )
+        setStudens(res.data.map( (students) =>({
+          ...students,
+          present : true
+      })))
+        
+      } catch (error) {
+         console.error(error)
+      }
+
+     }
+     fetchStudents()
+   } , [ user])
+
+   const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try { 
+       await api.post('/api/attendnace/markatendance' , {date ,
+         records : students.map( (student) =>(
+          {
+            studentId : student._id,
+            status : student.present ? 'present' : 'absent',
+          }
+         )),
+       });
+       alert('Attendance marked successfully')
+    } catch (error) {
+       console.error(error)
+    }
+   }
+ 
+ 
   return (
-    <div>AttendnaceMarking</div>
+    <>
+      <h2> Attendance Marking</h2>
+
+      <form  onSubmit={handleSubmit} >
+
+        <label>Date</label>
+        <input
+          type='date'
+          value = {date}
+          onChange={(e) =>{
+            setDate(e.target.value)
+          }}
+          required
+        
+        />
+
+
+           <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>RollNo</th>
+                <th>Present?</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map ( ( student) => (
+                  <tr key = {student._id}>
+                    <td>{student.email}</td>
+                    <td>{student.rollNo}</td>
+ 
+                   <td>
+                     <input  
+                       type = 'checkbox'
+                       checked = {student.present}
+                       onChange={(e) =>{
+                        const newStduents = [...students]
+                        const target =  newStduents.find(
+                          (s) =>  s._id === student._id
+                        );
+                        target.present = e.target.checked
+                        setStudens(newStduents)
+                       }}
+                       
+                     />
+
+                   </td>
+                  </tr>
+              ))}
+            </tbody>
+           </table>
+           <button
+            type='submit'
+           > Mark Attendance</button>
+      </form>
+    </>
   )
 }
 
